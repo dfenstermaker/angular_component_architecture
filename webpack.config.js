da78f43a10b66105ecfb0e947b9d730a83fce554
Danny Fenstermaker <dannyfenstermaker@gmail.com>
2016-01-01 14:24:38 -0700

    initial build

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+var webpack            = require('webpack'),
+    CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin"),
+    HtmlWebpackPlugin  = require('html-webpack-plugin'),
+    PrefetchPlugin     = webpack.PrefetchPlugin,
+    npmRoot            = __dirname + "/node_modules";
+
+module.exports = {
+  entry: {
+    vendors: [
+      'angular',
+      'lodash',
+      'angular-ui-router',
+      'angular-material'
+    ],
+    index: ['./src/index.js'],
+    styles: [
+      './src/main.scss'
+    ]
+  },
+
+  output: {
+    path: __dirname + "/target/webroot",
+    filename: '[name].js'
+  },
+
+  module: {
+    loaders: [{
+      test: /[\/]angular\.js$/,
+      loader: "exports?angular"
+    }, {
+      test: /\.js$/,
+      exclude: [/node_modules/],
+      loader: "babel?cacheDirectory=true&presets=es2015"
+    }, {
+      test: /\.html$/,
+      exclude: [/node_modules/],
+      loader: "html"
+    }, {
+      test: /main\.scss$/,
+      // Query parameters are passed to node-sass
+      loader: "style!css!sass?outputStyle=expanded"
+    },
+
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.svg$/, loader: "raw" }
+    ],
+
+    noParse: [
+    ]
+  },
+
+  port: 9000,
+
+  resolve: {
+    extensions: ['', '.js', '.json', '.scss', '.css'],
+    modulesDirectories: ['bower_components', 'node_modules', 'src'],
+    alias: {
+      module: 'module.js'
+    }
+  },
+
+  devtool: "source-map",
+
+  plugins: [
+    new PrefetchPlugin('angular'),
+    new PrefetchPlugin('lodash'),
+    new PrefetchPlugin('module'),
+    new webpack.optimize.DedupePlugin(),
+    new CommonsChunkPlugin("vendors", "vendors.[hash].js"),
+    new HtmlWebpackPlugin({
+      title: 'Components',
+      template: 'index.html',
+      inject: true
+    })
+  ]
+}
---
+++ webpack.server.js
@@ -0,0 +1,17 @@
+var server = require('webpack-dev-server')
+var config = require('./webpack.config.js')
+var webpack = require('webpack');
+
+var devConfig = Object.create(config)
+devConfig.watch = true
+
+var compiler = webpack(devConfig);
+
+webserver = new server(compiler, {
+    historyApiFallback: true,
+    stats: {colors: true}
+});
+
+webserver.listen(devConfig.port, '0.0.0.0', function () {});
+
+console.log('Running on port ' + devConfig.port);
