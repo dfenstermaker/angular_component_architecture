eb6955ebba16554accad277400ea43fe5954e27a
Danny Fenstermaker <dannyfenstermaker@gmail.com>
2016-01-01 20:57:15 -0700

    services module

--- src/module.js
+++ src/module.js
@@ -1,9 +1,11 @@
 import angular from 'angular'
 import home from './components/home/'
+import services from './services/'
 
 export default angular.module('Angular_Components', [
     'ui.router',
     'ngMaterial',
     'ngMdIcons',
+    services.name,
     home.name
 ])
---
+++ src/services/index.js
@@ -0,0 +1,10 @@
+import angular from 'angular'
+
+const configs = {
+    name: 'services_module'
+}
+
+angular.module(configs.name, [])
+    // .service('', )
+
+export default configs
